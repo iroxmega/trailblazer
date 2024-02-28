fc307c4066eaf3d82a8e94645dee2ab48f72a2ba
iroxmega <kirkin.danil2015@yandex.ru>
2024-02-28 08:19:54 +0400

    - new db slice

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import authReducer from './slices/authSlice.ts'
+import dbReducer from './slices/dbSlice.ts'
 export const store = configureStore({
     reducer: {
-        auth: authReducer
+        auth: authReducer,
+        db: dbReducer,
     },
 })
 
